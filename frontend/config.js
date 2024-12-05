378061b19cd4f2cc071506d94c8466123d2b45e0
MariamAzzouz <azzouzmariam3@gmail.com>
2024-12-05 16:45:18 +0100

    Add files via upload

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+export const TMDB_API_KEY = '2236b1394816474a4a0c9ce17cce84ea';
+export const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
